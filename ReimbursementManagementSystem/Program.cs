23163a47ad444d6de360617e85c54e151b173b45
Kristopher Holland <krisholl2022@gmail.com>
2022-06-24 11:02:01 -0400

    Breakdown of project beginning
    Signed-off-by: Kristopher Holland <krisholl2022@gmail.com>

---
+++ ReimbursementManagementSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Expense Reimbursement management
+
+    This is a management system where a user can either be an employee or an employer
+
+    An employee can:
+        submit a ticket asking for reimbursement
+        view status of ticket
+            maybe gets a notification for a change in ticket with a ticket number
+        
+    
+    A manager can:
+        view status of all tickets
+        change status of pending ticket
+            accepted
+            denied
+        cannot change any other ticket or any information other than state of a ticket
+
+    Maybe the ticket comes from a separte file so need to use System.file
+
+    Need a userId  for employees and managers
+
+    Need classes for a Users
+        string userID
+        string name
+        string 
+*/
