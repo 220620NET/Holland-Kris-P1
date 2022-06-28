b58cd8dc69fda02ecc0ba03a575271d28b242ed4
Kristopher Holland <krisholl2022@gmail.com>
2022-06-28 16:18:31 -0400

    Made Users class and Fixed outline for Tickets class. Next will be to create constructors.
    Signed-off-by: Kristopher Holland <krisholl2022@gmail.com>

--- RMS-P1/Models/Tickets.cs
+++ RMS-P1/Models/Tickets.cs
@@ -2,11 +2,13 @@ namespace Models;
 public enum Status{
     Pending,Approved,Denied
 }
+
+//Basic class for Tickets setting an Author and Resolver will require its own login
 public class Tickets{
     public string Author{get;set;}
     public int ID{get;set;}
     public string Resolver{get;set;}
     public string Description{get;set;}
-    public Status state{get;set;}
-    public decimal amount{get;set;}
+    public Status State{get;set;}
+    public decimal Amount{get;set;}
 }
---
+++ RMS-P1/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace Models;
+
+public enum Roles{
+    Employee, Manager
+}
+public class Users{
+    public Roles Role {get;set;}
+    public int UserID{get;set;}
+    public string Name{get;set;}
+    public string Password{get;set;}
+}
