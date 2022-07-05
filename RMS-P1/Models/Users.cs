8b6194bef6550353a86662514e802e1f0dcc1834
krisholl <krisholl2022@gmail.com>
2022-07-05 15:29:11 -0400

    Fixed users
    Signed-off-by: krisholl <krisholl2022@gmail.com>

--- RMS-P1/Models/Users.cs
+++ RMS-P1/Models/Users.cs
@@ -9,4 +9,18 @@ public class Users{
     public int UserID{get;set;}
     public string Name{get;set;}
     public string Password{get;set;}
+
+    public Users(int user, string Name, string password, Roles role){
+        UserID = user;
+        this.Name=Name;
+        Password=password;
+        Role = role;
+    }
+    public Users(string name, string password){
+        Name=name;
+        Password=password;
+    }
+    public override string ToString(){
+        return $"Name: {Name}, Password: {Password}";
+    }
 }
