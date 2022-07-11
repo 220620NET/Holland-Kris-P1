025b3b566dbdd746d6261cdf3c4186ea3bc85a41
krisholl <krisholl2022@gmail.com>
2022-07-11 16:02:30 -0400

    Created Ticket Services
    Signed-off-by: krisholl <krisholl2022@gmail.com>

---
+++ ExpenseManagement/Services/TicketServices.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+using DataAccess;
+using CustomExceptions;
+namespace Services
+{
+    public class TicketServices
+    {
+        //SubmitReimbursement
+        public bool SubmitReimbursement(Tickets newTicket)
+        {
+            try
+            {
+                return new TicketRepostitory().CreateTicket(newTicket);
+            }
+            catch (Exception)
+            {
+                throw;
+
+            }
+        }
+        //UpdateReimbursement
+        public bool UpdateReimbursement(Tickets update)
+        {
+            try
+            {
+                return new TicketRepostitory().UpdateTicket(update);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        //GetReimbursementByID
+        public Tickets GetReimbursementByID(int ticketID)
+        {
+            try
+            {
+                return new TicketRepostitory().GetTicketsById(ticketID);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        //GetReimbursementByUserId
+        public List<Tickets> GetReimbursementByUserID(int userID)
+        {
+            try
+            {
+                return new TicketRepostitory().GetTicketsByAuthor(userID);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        //GetReimbursementByStatus
+        public List<Tickets> GetReimbursementByStatus(Status state)
+        {
+            try
+            {
+                return new TicketRepostitory().GetTicketsByStatus(state);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
